750fcd074ba9bbdd34eb5742d4555afd821bc86b
parkinsonk <59220394+parkinsonk@users.noreply.github.com>
2021-03-25 16:44:36 -0600

    returns_graph uploads

--- Content/0005_returns_graph/returns_graph.R
+++ Content/0005_returns_graph/returns_graph.R
@@ -1,11 +1,9 @@
-library(tidyverse)
-library(tidyquant)
-library(lubridate)
-library(forcats)
-library(gridExtra)
-library(viridis)
-library(wesanderson)
-library(RColorBrewer)
+library(tidyverse) #Used for data wrangling and visualization
+library(tidyquant) #Get Stock data
+library(lubridate) #Deals with dates
+library(gridExtra) #Combine multiple plots
+library(viridis)   #For coloring the plot
+
 
 # Large Cap Index Returns - VV
 # Mid Cap Index Returns - VO
@@ -44,8 +42,10 @@ graph <- function(year) {
       symbol == 'VNQ' ~ 'REITs'),
       Year = as.factor(Year), symbol = as.factor(symbol), Returns = scales::percent(Returns, accuracy = .11))
   
+  #Reorders the levels of the symbols so that the with the greatest return appears at the top of the plot
   returns_yearly$symbol <- factor(returns_yearly$symbol, levels = returns_yearly$symbol)
   
+  #Creates return plots for years 2010 - 2020.
   ggplot(returns_yearly, aes(x = Year, y = symbol, fill = symbol)) +
     geom_tile() + 
     geom_text(data = returns_yearly, aes(x = Year, y = symbol, label = symbol, vjust = -.2, fontface = 'bold'), size =3.25) +
@@ -65,6 +65,7 @@ graph <- function(year) {
           panel.margin = unit(0,"null"))
 }
 
+#Averages the asset class returns from 2010 - 2020
 returns_yearly_avg <- prices %>%
   group_by(symbol) %>%
   tq_transmute(select     = adjusted, 
@@ -85,8 +86,10 @@ returns_yearly_avg <- prices %>%
     symbol == "VNQ" ~ "REITs"
     ),`10-Yr Average` = '10-Yr Average', Year = as.factor(`10-Yr Average`), symbol = as.factor(symbol), Averages = scales::percent(Averages, accuracy = .11))
 
+#Reorders the levels of the symbols so that the with the greatest return appears at the top of the plot
 returns_yearly_avg$symbol <- factor(returns_yearly_avg$symbol, levels = returns_yearly_avg$symbol)
 
+# Creates a 10-Year Average plot
 averages_plot <- ggplot(returns_yearly_avg, aes(x = `10-Yr Average`, y = symbol, fill = symbol)) +
   geom_tile() + 
   geom_text(data = returns_yearly_avg, aes(x = `10-Yr Average`, y = symbol, label = symbol, vjust = -.2, fontface = 'bold'), size = 3.25) +
@@ -106,7 +109,7 @@ averages_plot <- ggplot(returns_yearly_avg, aes(x = `10-Yr Average`, y = symbol,
         panel.border = element_rect(colour = "black", fill=NA, size=1.5),
         strip.background = element_rect(color = "black", size = 1.5))
 
-
+#Combines all plots.
 grid.arrange(graph(2010), graph(2011), graph(2012), graph(2013), graph(2014), graph(2015), 
              graph(2016), graph(2017), graph(2018), graph(2019), graph(2020), averages_plot, ncol=13)
 
