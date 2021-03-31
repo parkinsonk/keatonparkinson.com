2630b1bdfc709e418c69cc16a6d2d770672a8585
parkinsonk <59220394+parkinsonk@users.noreply.github.com>
2021-03-31 16:51:50 -0600

    upload sd_returns_graph

--- Content/0006_sd_returns_graph/sd_returns_graph.R
+++ Content/0006_sd_returns_graph/sd_returns_graph.R
@@ -17,7 +17,7 @@ library(ggthemes)
 
 #Get Stock prices from Yahoo Finance
 
-prices <- c("VV", 'VO', 'VB', 'VNQ', 'VWO', 'VEA', 'GLD') %>%
+prices <- c("VV", 'VO', 'VB', 'VNQ', 'VWO', 'VEA', 'GLD', 'BND') %>%
   tq_get(get  = "stock.prices",
          from = "2010-01-01",
          to   = "2021-01-01")
@@ -29,7 +29,11 @@ annualized_sd <- prices %>%
                mutate_fun = periodReturn, 
                period     = "monthly", 
                col_rename = "Returns") %>% 
-  summarize(annualized_sd = (sd(Returns) * sqrt(12)) * 100)
+  summarize(annualized_sd = (sd(Returns) * sqrt(12)) * 100) %>% 
+  arrange(desc(annualized_sd))
+
+#Reorders the levels of the symbols so that the with the greatest standard deviation appears at the top of the plot
+annualized_sd$symbol <- factor(annualized_sd$symbol, levels = annualized_sd$symbol)
 
 # Yearly Returns
 returns_weekly <- prices %>% 
@@ -40,6 +44,8 @@ returns_weekly <- prices %>%
                col_rename = "Returns") %>% 
   filter(Returns != max(Returns), Returns != min(Returns)) %>% 
   mutate(year = year(date), other_years = 'Other Yrs Returns')
+
+returns_weekly$symbol <- factor(returns_weekly$symbol, levels = annualized_sd$symbol)
 
 # Find the max yearly returns
 returns_weekly_max <- prices %>% 
@@ -65,6 +71,8 @@ max_year <- inner_join(returns_weekly_max, returns_weekly_max_yr, by = c('symbol
   select(symbol, max, year) %>% 
   mutate(Max = 'Max Return')
 
+max_year$symbol <- factor(max_year$symbol, levels = annualized_sd$symbol)
+
 # Find the min yearly returns
 returns_weekly_min <- prices %>% 
   group_by(symbol) %>%
@@ -88,6 +96,8 @@ min_year <- inner_join(returns_weekly_min, returns_weekly_min_yr, by = c('symbol
   select(symbol, min, year) %>% 
   mutate(Min = 'Max Loss')
 
+min_year$symbol <- factor(min_year$symbol, levels = annualized_sd$symbol)
+
 returns_weekly_median <- prices %>% 
   group_by(symbol) %>%
   tq_transmute(select     = adjusted, 
@@ -97,8 +107,11 @@ returns_weekly_median <- prices %>%
   summarize(Average = mean(Returns)) %>% 
   mutate(Averages = '10-Yr Average')
 
+returns_weekly_median$symbol <- factor(returns_weekly_median$symbol, levels = annualized_sd$symbol)
+
+
 ggplot() +
-  geom_point(returns_weekly, mapping = aes(x = '', y = Returns, color = other_years), alpha = .2) +
+  geom_point(returns_weekly, mapping = aes(x = '', y = Returns, color = other_years), alpha = .5) +
   geom_point(max_year, mapping = aes(x = '', y = max, color = Max), size = 5) +
   geom_text(max_year, mapping = aes(x = '', y = max, label = year), vjust = 2, size = 3.5) +
   geom_text(max_year, mapping = aes(x = '', y = max, label = scales::percent(max, .11)), vjust = -1, size = 3.5) +
@@ -112,12 +125,11 @@ ggplot() +
             vjust   = 1, size = 3.25, fontface = 'bold') +
   ylim(-.3, .5) +
   coord_flip() +
+  labs(x = '') +
+  scale_color_manual(values = c('blue1', 'red1', 'green1', 'black')) +
   facet_grid(rows = vars(symbol)) +
   theme(strip.text.y.left = element_text(angle = 0)) +
   theme_bw() +
-  theme(legend.position = "bottom")
-  
-
-returns_daily %>% 
-  group_by(symbol) %>% 
-  tq_performance(perfomance_fun = sd.annualized())
+  theme(legend.position = "bottom",
+        legend.title = element_blank(),
+        axis.ticks.y = element_blank())
